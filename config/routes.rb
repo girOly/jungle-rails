7e110051262156fa0e6805445855f6847b66744a
girOly <girardob@gmail.com>
2019-08-22 18:59:55 +0000

    Implemented Login Registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,36 @@
 Rails.application.routes.draw do
-
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: %i[index show]
   resources :categories, only: [:show]
-  get "/about", to:"about#show"
+  get '/about', to: 'about#show'
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
 
-  resources :orders, only: [:create, :show]
+  resources :orders, only: %i[create show]
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:index, :new, :create]  
-end
+    resources :products, except: %i[edit update show]
+    resources :categories, only: %i[index new create]
+  end
+
+  # config/routes.rb
+
+  # This route sends requests to our naked url to the *cool* action in the *gif* controller.
+
+  # I've created a gif controller so I have a page I can secure later.
+  # This is optional (as is the root to: above).
+
+  # These routes will be for signup. The first renders a form in the browse, the second will
+  # receive the form and create a user in our database using the data given to us by the user.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
